914511d6e1953723de0efb98eae6cea275991e33
HorstGab <gabriel.horst.m@gmail.com>
2019-08-29 21:06:05 -0300

    Create main.py

---
+++ Link opener/main.py
@@ -0,0 +1,16 @@
+import sys
+import os
+import re
+
+file_links_to_open = open('links_WIE.txt', 'r');
+
+def main():
+
+	for line in file_links_to_open:
+		print 'Trying to open ' + line;
+		os.system("start \"\" " + line);
+	pass
+
+if __name__ == '__main__':
+	main();
+	pass
